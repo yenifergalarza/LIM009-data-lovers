74f46e70648b98d6b04b1218b0e8e3011d3cc7a2
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-03-21 12:04:06 -0500

    readme e imoprime

--- src/main.js
+++ src/main.js
@@ -1,20 +1,36 @@
 /* Manejo del DOM */
 const  pokeData = POKEMON.pokemon;
-const arrayPoke = Object.values(pokeData);
+/*const arrayPoke = Object.values(pokeData); codigo muerto :(*/
 const printPoke = document.getElementById("root");
  
 const eachPoke = (pokemon) =>{ for(let pokeOne = 0; pokeOne< POKEMON.pokemon.length;pokeOne++){
-  console.log(POKEMON.pokemon[pokeOne].name);
-  printPoke.innerHTML +=`
-  <figure>
-  <img src="${pokemon[pokeOne].img}">
-  </figure>                
-  <p> Nombre : ${pokemon[pokeOne].name}</p>
-  <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
-  <p> Tipo : ${pokemon[pokeOne].type}</p>
-  <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
-  <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
-  <p> Caramelos : ${pokemon[pokeOne].candy_count}</p>
-   `;
+  if(POKEMON.pokemon[pokeOne].hasOwnProperty('candy_count'))
+  {
+    //console.log(.pokemon[pokeOne].name);
+    printPoke.innerHTML +=`
+    <figure>
+    <img src="${pokemon[pokeOne].img}">
+    </figure>                
+    <p> Nombre : ${pokemon[pokeOne].name}</p>
+    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
+    <p> Tipo : ${pokemon[pokeOne].type}</p>
+    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
+    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
+    <p> Caramelos : ${pokemon[pokeOne].candy_count}</p>
+     `
+  }
+  else{
+    printPoke.innerHTML +=`
+    <figure>
+    <img src="${pokemon[pokeOne].img}">
+    </figure>                
+    <p> Nombre : ${pokemon[pokeOne].name}</p>
+    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
+    <p> Tipo : ${pokemon[pokeOne].type}</p>
+    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
+    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
+    <p> Caramelos : no necesita mas ${pokemon[pokeOne].candy}</p>
+     `}
+  
 }}
-eachPoke(arrayPoke);
+eachPoke(pokeData);
