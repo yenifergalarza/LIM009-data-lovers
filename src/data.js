66ccac9b8f59e660380a004831894bb520cff52d
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-03-22 11:23:29 -0500

    añadiendo bootstrap css

--- src/data.js
+++ src/data.js
@@ -3,15 +3,7 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-/* const example = () => {
-  for (name of Object.values(POKEMON)) {
-    console.log(name); 
-    }
-};
+/*
   for (name of Object.values(POKEMON)) {
   console.log(Object.values(name)); 
-
-
-}
-window.example = example;
  */
--- src/main.js
+++ src/main.js
@@ -1,36 +1,43 @@
 /* Manejo del DOM */
-const  pokeData = POKEMON.pokemon;
+
 /*const arrayPoke = Object.values(pokeData); codigo muerto :(*/
-const printPoke = document.getElementById("root");
- 
-const eachPoke = (pokemon) =>{ for(let pokeOne = 0; pokeOne< POKEMON.pokemon.length;pokeOne++){
-  if(POKEMON.pokemon[pokeOne].hasOwnProperty('candy_count'))
-  {
-    //console.log(.pokemon[pokeOne].name);
-    printPoke.innerHTML +=`
-    <figure>
-    <img src="${pokemon[pokeOne].img}">
-    </figure>                
-    <p> Nombre : ${pokemon[pokeOne].name}</p>
-    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
-    <p> Tipo : ${pokemon[pokeOne].type}</p>
-    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
-    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
-    <p> Caramelos : ${pokemon[pokeOne].candy_count}</p>
-     `
+const printPoke = document.getElementById("rootCard");
+const pokeData = POKEMON.pokemon;
+const eachPoke = (pokemon) => {
+  for (let pokeOne = 0; pokeOne < POKEMON.pokemon.length; pokeOne++) {
+    if (POKEMON.pokemon[pokeOne].hasOwnProperty('candy_count')) {
+      //console.log(.pokemon[pokeOne].name);
+      printPoke.innerHTML += `
+      <div class="card">
+    <img src="${pokemon[pokeOne].img}" class="card-img-top" > 
+    <div class="card-body card-header">
+    <p class="card-title"> Nombre : ${pokemon[pokeOne].name}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item" > N° de Pokemon : ${pokemon[pokeOne].num}</li>
+    <li class="list-group-item" > Tipo : ${pokemon[pokeOne].type}</li>
+    <li class="list-group-item"  > Debilidades : ${pokemon[pokeOne].weaknesses}</li>
+    <li class="list-group-item" > Apariciones : ${pokemon[pokeOne].spawn_chance}</li>
+    <li class="list-group-item" > Caramelos : ${pokemon[pokeOne].candy_count}</li>
+    </ul>
+    </div> `
+    } else {
+      printPoke.innerHTML += `
+      <div class="card" style="width: 12rem;">
+    <img src="${pokemon[pokeOne].img}" class="card-img-top" > 
+    <div class="card-body card-header">
+    <p class="card-title"> Nombre : ${pokemon[pokeOne].name}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item" > N° de Pokemon : ${pokemon[pokeOne].num}</li>
+    <li class="list-group-item" > Tipo : ${pokemon[pokeOne].type}</li>
+    <li class="list-group-item"  > Debilidades : ${pokemon[pokeOne].weaknesses}</li>
+    <li class="list-group-item" > Apariciones : ${pokemon[pokeOne].spawn_chance}</li>
+    <li class="list-group-item" > Caramelos : no necesita mas ${pokemon[pokeOne].candy} </li>
+    </ul>
+    </div> `
+    }
+
   }
-  else{
-    printPoke.innerHTML +=`
-    <figure>
-    <img src="${pokemon[pokeOne].img}">
-    </figure>                
-    <p> Nombre : ${pokemon[pokeOne].name}</p>
-    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
-    <p> Tipo : ${pokemon[pokeOne].type}</p>
-    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
-    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
-    <p> Caramelos : no necesita mas ${pokemon[pokeOne].candy}</p>
-     `}
-  
-}}
+}
 eachPoke(pokeData);
