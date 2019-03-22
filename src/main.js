8689187c1b9af83d472137726604486d5b881b6d
Mirella1321 <47748876+Mirella1321@users.noreply.github.com>
2019-03-22 11:44:09 -0500

    main ya actualizado con bootstrap

--- src/main.js
+++ src/main.js
@@ -1,41 +1,4 @@
 /* Manejo del DOM */
-<<<<<<< HEAD
-const  pokeData = POKEMON.pokemon;
-const printPoke = document.getElementById("root");
- 
-const eachPoke = (pokemon) =>{ for(let pokeOne = 0; pokeOne< POKEMON.pokemon.length;pokeOne++){
-  if(POKEMON.pokemon[pokeOne].hasOwnProperty('candy_count'))
-  {
-    //console.log(.pokemon[pokeOne].name);
-    printPoke.innerHTML +=`
-    <figure>
-    <img src="${pokemon[pokeOne].img}">
-    </figure>                
-    <p> Nombre : ${pokemon[pokeOne].name}</p>
-    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
-    <p> Tipo : ${pokemon[pokeOne].type}</p>
-    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
-    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
-    <p> Caramelos : ${pokemon[pokeOne].candy_count}</p>
-     `
-  }
-  else{
-    printPoke.innerHTML +=`
-    <figure>
-    <img src="${pokemon[pokeOne].img}">
-    </figure>                
-    <p> Nombre : ${pokemon[pokeOne].name}</p>
-    <p> N° de Pokemon : ${pokemon[pokeOne].num}</p>
-    <p> Tipo : ${pokemon[pokeOne].type}</p>
-    <p> Debilidades : ${pokemon[pokeOne].weaknesses}</p>
-    <p> Apariciones : ${pokemon[pokeOne].spawn_chance}</p>
-    <p> Caramelos : no necesita mas ${pokemon[pokeOne].candy}</p>
-     `}
-  
-}}
-eachPoke(pokeData);
-=======
-
 /*const arrayPoke = Object.values(pokeData); codigo muerto :(*/
 const printPoke = document.getElementById("rootCard");
 const pokeData = POKEMON.pokemon;
@@ -77,4 +40,3 @@ const eachPoke = (pokemon) => {
   }
 }
 eachPoke(pokeData);
->>>>>>> 66ccac9b8f59e660380a004831894bb520cff52d
