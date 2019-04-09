760b2f8f1073ff35640ff5e44dc6d2c0c5d2e98e
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-04-09 18:32:25 -0500

    con fav icon

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,26 +1,38 @@
 global.window = global;
-global.assert = require('chai').assert;
+
 require('../src/data.js');
 require('./data.spec.js');
 
 
 describe('sortArrsToObjects', () => {
-  
+
   it('debería ser una función', () => {
     expect(typeof sortArrsToObjects).toBe('function');
   });
 
   it('debería retornar -1 por que el primer parametro es el menor', () => {
-    expect(sortArrsToObjects({id:5},{id:8})).toBe(-1)
+    expect(sortArrsToObjects({
+      id: 5
+    }, {
+      id: 8
+    })).toBe(-1)
   });
 
   it('debería retornar 1 por que el primer parametro es mayor', () => {
-    expect(sortArrsToObjects({id:8},{id:1})).toBe(1)
+    expect(sortArrsToObjects({
+      id: 8
+    }, {
+      id: 1
+    })).toBe(1)
   });
 
 
   it('debería retornar 0 por que los 2 parametros son iguales', () => {
-   expect(sortArrsToObjects({id:8},{id:8})).toBe(0)
+    expect(sortArrsToObjects({
+      id: 8
+    }, {
+      id: 8
+    })).toBe(0)
   });
 });
 describe('filtrarPokemon', () => {
@@ -29,7 +41,7 @@ describe('filtrarPokemon', () => {
   });
 
   test('deberia retornar pokes filtrados', () => {
-    expect(filtrarPokemon([{type:"fire"},{type:"ghost"},{id:1,type:"Fire"}],"Fire")).toEqual([])
+    expect(filtrarPokemon([{type: "fire"}, { type: "ghost" }, { id: 1,type: "Fire"}], "Fire")).toEqual([])
   });
 
 
@@ -37,7 +49,15 @@ describe('filtrarPokemon', () => {
 
 describe('filtrarPokemon', () => {
   it('deberia retornar % pokes de un tipo del total', () => {
-    expect(calculusStats([{type:"fuego"},{type:"fuego"},{type:"agua"}],[{type:"agua"}])).toBe(0.03)
+    expect(calculusStats([{
+      type: "fuego"
+    }, {
+      type: "fuego"
+    }, {
+      type: "agua"
+    }], [{
+      type: "agua"
+    }])).toBe(0.03)
   });
 
 
